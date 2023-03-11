41701e29f14ad6198ab31c7a3825380958bb87ec
Surv <83309545+SurvExE-Pc@users.noreply.github.com>
2023-03-11 22:50:09 +0600

    e

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>WebSock</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="WebSock" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Very real page
         </p>
       </main>
 
